b1aaa3a983f06ceeb51587c228a1de77da7fa329
JBreizh <drolante2000@gmail.com>
2023-04-12 11:41:25 +0200

    modify App.js to test update with CI/CD

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+	  Coucou c'est JBreizh
+	</p>
+	<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
