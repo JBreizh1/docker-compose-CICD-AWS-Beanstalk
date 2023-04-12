e93a0e2dad6f1337a99dff864b0f4f67637c0e80
JBreizh <drolante2000@gmail.com>
2023-04-12 14:53:43 +0200

    instancier docker sur un nouvelle appli AWS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Coucou c'est JBreizh qui met a jour
+	  Re Coucou c'est JBreizh qui met a jour
 	</p>
 	<p>
           Edit <code>src/App.js</code> and save to reload.
